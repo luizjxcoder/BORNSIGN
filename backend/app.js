fa8f190773159c71f4f7d3a422ac6bafa60acb07
luizjxcoder <jxcoder.dev@gmail.com>
2023-11-30 11:13:26 -0300

    app.js

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-const { escape } = require("querystring");
-const pot = 5000;
+const port = 5000;
 
-const app = exress()
+const app = express()
 
 // config JSON and  forma data response
 app.use(express.json())
-app.use(express.urlencoded({extend: false}))
+app.use(express.urlencoded({ extend: false }));
+
+app.listen(port, () => {
+     console.log(`App rodando na porta ${port}`);
+});
 
