9801eaa28306ba6ac4547ec08420e3ed0ebf4080
luizjxcoder <jxcoder.dev@gmail.com>
2023-12-02 10:20:57 -0300

    routes

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,28 @@
+require("dotenv").config()
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-const port = 5000;
+     
+const port = process.env.PORT;
 
 const app = express()
 
 // config JSON and  forma data response
 app.use(express.json())
 app.use(express.urlencoded({ extend: false }));
+
+//Solve CORS
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
+//Upload directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+//DB connection - conexão com banco de dados
+require("./config/db.js");
+
+// routes
+const router = require("./routes/Router.js")
+app.use(router);
 
 app.listen(port, () => {
      console.log(`App rodando na porta ${port}`);
