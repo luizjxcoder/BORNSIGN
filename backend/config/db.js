c0faab2bdb9414668ad026ef356a577f19750e5d
luizjxcoder <jxcoder.dev@gmail.com>
2023-12-02 12:16:30 -0300

    banco connect

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,22 @@
 const mongoose = require("mongoose");
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
 
-// connection
+const conn = async () => {
+     try {
+
+          const dbConn = await mongoose.connect
+          (`mongodb+srv://${dbUser}:${dbPassword}@bornsign.ien8lzp.mongodb.net/
+`);
+          
+          console.log("conectou ao Banco!");
+
+          return dbConn;
+     } catch (error) {
+
+          console.log(error);
+     };
+};
+
+conn();
+module.exports = conn;
