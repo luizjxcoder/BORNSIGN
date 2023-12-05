437be06ff72cf1f2d93fc350b81a17206dea84af
luizjxcoder <jxcoder.dev@gmail.com>
2023-12-05 12:42:13 -0300

    validação user

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,10 +1,14 @@
+// ARQUIVO DE ROTAS
 const express = require("express");
 const router = express.Router();
 
 //Controller
-const { register } = require("../controllers/UserController.js")
+const { register } = require("../controllers/UserController.js");
+
+//Middlewares
+const validate = require("../middlewares/handleValidation");
 
 //Routes
-router.post("/register", register);
+router.post("/register", validate, register);
 
 module.exports = router;
